1531d2d9b2d2119685136657612c4692fe0b590a
Amir Abu Hasan <amirabuhasan@gmail.com>
2022-11-29 16:18:15 +0800

    add logic to filter by label

--- src/components/IssueItem.jsx
+++ src/components/IssueItem.jsx
@@ -2,6 +2,7 @@ import { GoIssueOpened, GoIssueClosed, GoComment } from 'react-icons/go';
 import { Link } from 'react-router-dom';
 import { relativeDate } from '../helpers/relativeDate';
 import { useUserData } from '../helpers/useUserData';
+import { Label } from './Label';
 
 export function IssueItem({
   assignee,
@@ -29,12 +30,7 @@ export function IssueItem({
       <div className="issue-content">
         <span>
           <Link to={`/issue/${number}`}>{title}</Link>
-          {labels &&
-            labels.map(label => (
-              <span className={`label red`} key={label}>
-                {label}
-              </span>
-            ))}
+          {labels && labels.map(label => <Label key={label} label={label} />)}
           <div>
             <small>
               #{number} opened {relativeDate(createdDate)}{' '}
